29001ce61e16da1cec7027fdd33e76a9fd68839b
daviaquino87 <davi26031@gmail.com@gmail.com>
2023-12-13 14:23:03 -0300

    teste da outra

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,15 @@ import { Module } from '@nestjs/common';
 import { CategoriesModule } from './categories/categories.module';
 import { PrismaModule } from './common/database/prisma.module';
 
+function teste() {
+  console.log();
+}
+
 @Module({
   imports: [CategoriesModule, PrismaModule],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    teste();
+  }
+}
